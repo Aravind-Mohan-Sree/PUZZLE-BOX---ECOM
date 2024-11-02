2dbcbc7b7bd9ec8a4bcad04c0c3c395cc2994e8a
Aravind-Mohan-Sree <mailuraravindms@gmail.com>
2024-11-02 21:05:50 +0530

    ''

--- controller/adminController/couponController.js
+++ controller/adminController/couponController.js
@@ -1,6 +1,7 @@
 const couponSchema = require('../../model/couponSchema');
+const voucherCodes = require('voucher-code-generator');
 
-/* ---------------------------------- will ---------------------------------- */
+/* ---------------------------------- will render the coupon page ---------------------------------- */
 const getCoupon = (req, res) => {
   try {
 
@@ -10,17 +11,55 @@ const getCoupon = (req, res) => {
       categories: []
     });
   } catch (err) {
-    console.log();
+    console.log('Error while rendering the coupon page', err);
   }
 };
 /* -------------------------------------------------------------------------- */
 
-/* ---------------------------------- will ---------------------------------- */
-const addCoupon = (req, res) => {
+/* ---------------------------------- will add new coupon ---------------------------------- */
+const addCoupon = async (req, res) => {
   try {
+    /* --------------------- will parse the date from input --------------------- */
+    function parseDate(dateString) {
+      const [day, month, year] = dateString.split('/').map(Number);
 
+      return new Date(year, month - 1, day);
+    }
+
+    /* ---------- generate a voucher code with specific options ---------- */
+    const couponCode = voucherCodes.generate({
+      length: 8,
+      count: 1,
+      charset: voucherCodes.charset("alphanumeric"),
+      prefix: "PUZZLE-",
+      postfix: "-BOX",
+      pattern: "####-####",
+    });
+
+    /* ----------- check if a coupon with the same name already exists ---------- */
+    const existingCoupon = await Coupon.aggregate([
+      {
+        $match: { couponName: couponName }
+      }
+    ]);
+
+    if (existingCoupon.length > 0) {
+      return res.status(400).json({ error: "Coupon already exists!" });
+    }
+
+    /* ------- if no coupon with the same name exists, add the new coupon ------- */
+    await couponSchema.create({
+      couponName,
+      couponCode,
+      discount: discountAmount,
+      expiryDate: parseDate(expiryDate),
+      minAmount: minimumAmount,
+    });
+
+    req.flash('alert', { message: 'Coupon created successfully!', color: 'bg-success' });
+    res.status(201).json({ success: true });
   } catch (err) {
-    console.log();
+    console.log('Error while adding new coupon', err);
   }
 };
 /* -------------------------------------------------------------------------- */
